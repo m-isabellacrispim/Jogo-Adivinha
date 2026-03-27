ea3f5775efceefc43bfb3f87c624b58f77fefb0b
Maria Isabella Crispim Teixeira <bellacrispim20@gmail.com>
2026-03-27 15:16:55 -0300

    Base do jogo pronta
    Consertei o bug que tinha nas tentativas e agora a base principal do jogo esta pronta.

--- jogo/jogo.c
+++ jogo/jogo.c
@@ -6,60 +6,56 @@ int main() {
 
     srand(time(NULL));
 
-    int numeroM = rand() % 100 + 1;
+    int randNum = rand() % 100 + 1;
     int tentar;
     int tentativa = 0;
     
 
     printf("==========JOGO DE ADIVINHAÇAO==========\n");
     printf("Regras:\n");
-    printf("1- nivel Facil = 7 tentativas\n");
-    printf("2- nivel Medio = 7 tentativas\n");
-    printf("3- nivel Dificil = 10 tentativas\n");
+    printf("1- nivel Facil(1-100) = 7 tentativas\n");
+    printf("2- nivel Medio(1-300) = 7 tentativas\n");
+    printf("3- nivel Dificil(1-500) = 7 tentativas\n");
     printf("=======================================\n");
     printf("\n");
     
 
     printf("Tente adivinhar o numero.\n");
     scanf("%d", &tentar);
-
-    
+    tentativa++;
         
-        while (numeroM != tentar && tentativa <= 5) {
+        while (randNum != tentar && tentativa < 7) {
             
-            if(numeroM < tentar){
+            if(randNum < tentar){
                 printf("===MENOR===\n");
             }
-            else if (numeroM > tentar){
+            else if (randNum > tentar){
                 printf("===MAIOR===\n");
             }
             
             printf("-->Tente novamenete.\n\n");
-            scanf("%d", &tentar);
-            
             tentativa++;
-            
+            scanf("%d", &tentar);
         }
     
     
-    if(tentativa > 5){
+    if(tentativa >= 7 && tentar != randNum){
         printf("Voce perdeu!\n");
-        printf("O numero escolhido era: %d\n", numeroM);
+        printf("O numero escolhido foi: %d\n", randNum);
         printf("Você tentou %d vezes.\n", tentativa);
-        
+    }
+    else if (tentativa == 1){
+        printf("Parabens!! Você acertou de primeira\n");
+        printf("O numero escolhido foi: %d", randNum);
+    }
+    else if(tentativa == 7 && tentar == randNum){
+        printf("Parabens!! Essa foi por pouco, você acertou na ultima tentativa\n");
+        printf("O numero escolhido foi: %d", randNum);
     }
     else{
-
-        if (tentativa == 1){
-            printf("Parabens!! Você acertou de primeira\n");
-            printf("O numero escolhido era: %d", numeroM);
-        }
-        else{
-            printf("Parabens!! Você acertou com %d tentativas.\n", tentativa);
-            printf("O numero escolhido era: %d", numeroM);
-        }
+        printf("Parabens!! Você acertou com %d tentativas.\n", tentativa);
+        printf("O numero escolhido foi: %d", randNum);
     }
-        
-        
+    
     return 0;
 }
